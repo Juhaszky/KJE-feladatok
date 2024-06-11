82128912bd409bd5ab050483d91b845e7be6dbc4
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-06-11 23:19:54 +0200

    Remove unnecessary routes

--- 04_automatizated_99_oam/app.js
+++ 04_automatizated_99_oam/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const filesRouter = require('./routes/files');
 
 var app = express();
@@ -20,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/files', filesRouter);
 
 // catch 404 and forward to error handler
